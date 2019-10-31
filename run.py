ad29bee97e50ded5d5ca80e6a3582dcce8cdecea
Ubiratan Neto <ubiratanneto37@hotmail.com>
2019-10-30 21:58:28 -0300

    Change piece type argument to piece size

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ def main():
   parser.add_argument('-i', '--input_path', dest='input_path', metavar='', type=str, default='examples/lena.png', help='Path to the input image. Default is \'examples/lena.png\'')
   parser.add_argument('-n', '--n_moving_pieces', dest='n_moving_pieces', metavar='', type=int, default=3, help='Number of pieces that are going to be moved. If the number given is bigger than the number of pieces, the default behaviour is to assume n_moving_pieces = number of pieces in the puzzle. Default number is 3.')
   parser.add_argument('-o', '--output_path', dest='output_path', metavar='', type=str, default='output.png', help='Path to save the output image (directory and name). Make sure that the directory exists. Default is \'output.png\'.')
-  parser.add_argument('-t', '--type', dest='type', metavar='', type=str, default='medium', help='Size of the puzzle pieces. The choices are: small for 32x32, medium for 64x64, big for 128x128.')
+  parser.add_argument('-p', '--piece_size', dest='piece_size', metavar='', type=int, default='64', help='Size of the puzzle pieces. This number must be bigger or equal than 32. Default is 64.')
 
   args = parser.parse_args()
 
@@ -34,16 +34,10 @@ def main():
     assert (original_image is not None), 'Image does not exist.'
 
     # Check if the piece type exists and get the piece size
-    assert (args.type in ['big', 'small', 'medium']), 'Type of piece size does not exist.'
-    if args.type == 'small':
-      piece_size = 32
-    elif args.type == 'big':
-      piece_size = 128
-    else:
-      piece_size = 64
+    assert (args.piece_size >= 32), 'Piece size must be greater or equal than 32.'
 
     # Check if image size is greater than two times the piece size
-    assert ((original_image.shape[0] >= 2*piece_size and original_image.shape[1] >= piece_size) or (original_image.shape[0] >= piece_size and original_image.shape[1] >= 2*piece_size)), 'Input image has smaller dimensions than the puzzle piece. Make sure that the image can fit at least two puzzle pieces.'
+    assert ((original_image.shape[0] >= 2*args.piece_size and original_image.shape[1] >= args.piece_size) or (original_image.shape[0] >= args.piece_size and original_image.shape[1] >= 2*args.piece_size)), 'Input image has smaller dimensions than the puzzle piece. Make sure that the image can fit at least two puzzle pieces.'
 
     # Check if number of moved pieces is greater or equal than zero
     assert (args.n_moving_pieces >= 0), 'Number of pieces to be moved needs to be greater or equal than zero.'
@@ -53,7 +47,7 @@ def main():
     exit(1)
   
   # Create the puzzle mask and the puzzle image
-  puzzle_image, puzzle_mask = puzzle_creation.create(original_image, piece_size)
+  puzzle_image, puzzle_mask = puzzle_creation.create(original_image, args.piece_size)
 
   # If the user does not give a correct path to the background, create a white background image
   if background_image is None:
@@ -67,7 +61,7 @@ def main():
     exit(1)
 
   # Transform image
-  puzzle_image, puzzle_mask, foreground_mask = transformations.transform_v1(puzzle_image, puzzle_mask, piece_size, background_image.shape, args.n_moving_pieces)
+  puzzle_image, puzzle_mask, foreground_mask = transformations.transform_v1(puzzle_image, puzzle_mask, args.piece_size, background_image.shape, args.n_moving_pieces)
 
   # Add background to image
   puzzle_image = effects.add_background(background_image, puzzle_image, foreground_mask)
